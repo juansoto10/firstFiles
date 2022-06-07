2a4a4c2a9bdfcd2821693eb5e617cc1a4fe194e4
juansoto10 <jpsoto121@gmail.com>
2022-06-07 15:15:39 -0500

    Just created .gitignore file and modified hangman_game_2.py -new words added-

--- video_games/hangman_game/hangman_game_2.py
+++ video_games/hangman_game/hangman_game_2.py
@@ -2,40 +2,39 @@ import random
 import time
  
 ## Listas de Palabras
-marcas_de_coches = ["ford", "audi", "nissan", "renault", "chevrolet", "mitsubishi", "cadillac", "suzuki"]
+marcas_de_carros = ["ford", "audi", "nissan", "renault", "chevrolet", "mitsubishi", "cadillac", "suzuki", "subaru", "jaguar", "ferrari", "fiat", "citroen", "volvo", "mitsubishi", "honda", "mazda", "volskwagen", "toyota", "hyundai", "isuzu", "lotus", "porsche", "peugeot", "jeep", "bugatti", "lamborghini", "lexus", "maserati", "tesla", "infiniti", "bentley", "daewoo", "dodge", "pontiac", "mclaren"]
  
-nombres_de_paises = [ "mexico", "colombia", "canada", "venezuela", "uruguay", "argentina",
-                      "guatemala", "panama", "ecuador"]
+nombres_de_paises = ["mexico", "colombia", "canada", "venezuela", "uruguay", "argentina", "guatemala", "panama","ecuador", "dinamarca", "suecia", "islandia", "francia", "italia", "turquia", "rusia", "ucrania", "polonia", "china", "tailandia", "indonesia", "noruega", "belgica", "rumania", "irlanda", "sudafrica", "egipto", "israel", "españa", "portugal", "vietnam", "grecia", "marruecos", "angola", "madagascar", "croacia", "suiza", "austria", "hungria", "estonia", "finlandia", "mongolia", "japon", "filipinas", "siria", "jordania", "libano", "singapur", "malasia", "australia", "belice", "bolivia", "chile", "nicaragua", "surinam"]
  
  
-print("Bienvenido al juego de ahorcado")
-time.sleep(2)
-print("El objetivo del juego es adivinar la palabra secreta letra por letra")
-print("tienes 5 vidas, pierdes una vida cada que te equivocas si te quedas sin vidas pierdes")
-time.sleep(2)
+print("Bienvenido al juego del ahorcado")
+time.sleep(1)
+print("El objetivo del juego es adivinar la palabra secreta letra por letra.")
+print("Tienes 5 vidas y pierdes una cada vez que te equivocas. Si te quedas sin vidas pierdes")
+time.sleep(3)
  
-print("Desea jugar con palabras de que son marcas de coches o nombres de paises")
-time.sleep(2)
+print("¿Deseas jugar con marcas de carros o nombres de países?")
+time.sleep(1)
  
-cat_seleccionada = input("Ingrese C para marcas de coches y P para nombres de paises: ")
+cat_seleccionada = input("Ingresa C para marcas de carros o P para nombres de países: ")
  
 while True:
     if cat_seleccionada.lower() == "c":
-        print("excelente a seleccionado marcas de coches")
-        palabra_secreta = random.choice(marcas_de_coches)
+        print("Has seleccionado marcas de carros")
+        palabra_secreta = random.choice(marcas_de_carros)
         break
     elif cat_seleccionada.lower() == "p":
-        print("excelente a seleccionado nombres de paises")
+        print("Has seleccionado nombres de países")
         palabra_secreta = random.choice(nombres_de_paises)
         break
  
     else:
-        print("Por favor seleccione una categoria valida")
-        cat_seleccionada = input("Ingrese C para marcas de coches y P para nombres de paises: ")
+        print("Por favor selecciona una categoría válida")
+        cat_seleccionada = input("Ingresa C para marcas de carros o P para nombres de países: ")
  
  
  
-## Es el numero de veces que se puede eqivocar
+# Es el numero de veces que se puede eqivocar
 vidas = 5
  
 lista_letras_adivinadas = []
@@ -48,24 +47,31 @@ while True:
     while True:
         letra_adivinada = input("Adivina una letra: ")
         if(len(letra_adivinada)!=1 or letra_adivinada.isnumeric()):
-            print("Eso no es una letra intenta con una sola letra")
+            print("Eso no es una letra, intenta con una sola letra.")
         else:
             if letra_adivinada.lower() in lista_letras_adivinadas:
-                print("Ya habias intentado con esa letra intenta con otra por favor")
+                print("Ya has intentado con esa letra,prueba otra por favor.")
             else:
                 lista_letras_adivinadas.append(letra_adivinada)
  
                 if letra_adivinada.lower() in palabra_secreta:
-                    print("Felicidades adivinaste una letra")
+                    print("Felicidades, adivinaste una letra")
                     break
                 else:
                     vidas = vidas-1
-                    print("Te haz equivocado y perdido una vida")
-                    print("Te quedan " + str(vidas) + " vidas")
+                    print("Te has equivocado. ¡Perdiste una vida!")
+                    print("Te quedan " + str(vidas) + " vidas...")
                     break
  
     if vidas==0:
-        print("Haz perdido la palabra secreta era: "+ palabra_secreta)
+        print("""
+ ▐▄▄▄ ▄▄▄·.▄▄ ·  ▌ ▐· ▐▄▄▄ ▄▄▄·.▄▄ ·  ▌ ▐· ▐▄▄▄ ▄▄▄·.▄▄ ·  ▌ ▐·
+  ·██▐█ ▄█▐█ ▀. ▪█·█▌  ·██▐█ ▄█▐█ ▀. ▪█·█▌  ·██▐█ ▄█▐█ ▀. ▪█·█▌
+▪▄ ██ ██▀·▄▀▀▀█▄▐█▐█•▪▄ ██ ██▀·▄▀▀▀█▄▐█▐█•▪▄ ██ ██▀·▄▀▀▀█▄▐█▐█•
+▐▌▐█▌▐█▪·•▐█▄▪▐█ ███ ▐▌▐█▌▐█▪·•▐█▄▪▐█ ███ ▐▌▐█▌▐█▪·•▐█▄▪▐█ ███ 
+ ▀▀▀•.▀    ▀▀▀▀ . ▀   ▀▀▀•.▀    ▀▀▀▀ . ▀   ▀▀▀•.▀    ▀▀▀▀ . ▀  
+""")
+        print("Has perdido 😔. La palabra secreta era: " + palabra_secreta.capitalize())
         break
  
  
@@ -86,6 +92,13 @@ while True:
  
  
     if letras_faltantes == 0:
-        print("Felicidades has ganado")
-        print("La palabra secreta es: " + palabra_secreta)
+        print("""
+ ▐▄▄▄ ▄▄▄·.▄▄ ·  ▌ ▐· ▐▄▄▄ ▄▄▄·.▄▄ ·  ▌ ▐· ▐▄▄▄ ▄▄▄·.▄▄ ·  ▌ ▐·
+  ·██▐█ ▄█▐█ ▀. ▪█·█▌  ·██▐█ ▄█▐█ ▀. ▪█·█▌  ·██▐█ ▄█▐█ ▀. ▪█·█▌
+▪▄ ██ ██▀·▄▀▀▀█▄▐█▐█•▪▄ ██ ██▀·▄▀▀▀█▄▐█▐█•▪▄ ██ ██▀·▄▀▀▀█▄▐█▐█•
+▐▌▐█▌▐█▪·•▐█▄▪▐█ ███ ▐▌▐█▌▐█▪·•▐█▄▪▐█ ███ ▐▌▐█▌▐█▪·•▐█▄▪▐█ ███ 
+ ▀▀▀•.▀    ▀▀▀▀ . ▀   ▀▀▀•.▀    ▀▀▀▀ . ▀   ▀▀▀•.▀    ▀▀▀▀ . ▀  
+""")
+        print("Felicidades, ¡has ganado! 😎")
+        print("La palabra secreta es: " + palabra_secreta.capitalize())
         break
